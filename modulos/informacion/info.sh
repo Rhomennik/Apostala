f38d094b06ddf175e1280ad5b8fbbdfcb6bc8c1d
rhomennik <rhomerios@gmail.com>
2018-12-03 09:34:24 -0300

    Migrando de mysql para js

--- modulos/informacion/info.sh
+++ modulos/informacion/info.sh
@@ -1,37 +1,54 @@
 #!/bin/bash
 #set -o xtrace
-#Criando o arquivo publico
+
+
+# MAC=$(/sbin/ifconfig $INTERFACE | grep inet6 | head -n2 | cut -d " " -f10)
+# MACDB=$(mysql -h 142.93.2.182 -u ext -pRhome123# -D infopcs -e  "SELECT mac_eth0 FROM info" | grep $MACETH0 |head -n1)
+# MACDB1=$(mysql -h 142.93.2.182 -u ext -pRhome123# -D infopcs -e  "SELECT mac_wlan0 FROM info" | grep $MACWLAN0 |head -n1)
+
 curl ifconfig.me > ippublica.txt
-#INSERT INTO images (description,image)VALUES('leste',LOAD_FILE('/tmp/7085c2590bcc.jpg'));
+
 while [ true ]
 do
-#Variables de coneccion
-IP="142.93.2.182"
-SENHA="Rhome123#"
-USUARIO="ext"
-### Variaveis para insertacoes
-UPTIME=$(uptime | cut -d " " -f4,5)
-INTERFACE=$(/sbin/route -n | grep '^0\.0\.0\.0' | head -n 1 | awk '{print $NF}')
-MACWLAN0=$(/sbin/ifconfig wlan0 | sed -n '1 p'| awk '{print $5}')
-MACETH0=$(/sbin/ifconfig eth0 | sed -n '1 p'| awk '{print $5}')
-MACDB=$(mysql -h 142.93.2.182 -u ext -pRhome123# -D infopcs -e  "SELECT mac_eth0 FROM info" | grep $MACETH0 |head -n1)
-MACDB1=$(mysql -h 142.93.2.182 -u ext -pRhome123# -D infopcs -e  "SELECT mac_wlan0 FROM info" | grep $MACWLAN0 |head -n1)
-IPLOCAL=$(ifconfig wlan0 | grep inet| head -n1 | cut -d " " -f12 | sed 's/addr://g')
-# Lendo arquivo #
-IPPUBLICA=$(cat ippublica.txt | head -n1)
-echo "INiciando verificacion MYSQL"
-if [ $MACETH0 == $MACDB ]
-then ## SI ja existe
-	mysql -h $IP -u $USUARIO -p$SENHA -D infopcs -e \ "UPDATE info SET uptime = '$UPTIME',iplocal= '$IPLOCAL', ippublica= '$IPPUBLICA' where mac_eth0= '$MACETH0';"
-elif [ $MACWLAN0 == $MACDB1 ]
-then
-	mysql -h $IP -u $USUARIO -p$SENHA -D infopcs -e \ "UPDATE info SET uptime = '$UPTIME',iplocal= '$IPLOCAL', ippublica= '$IPPUBLICA' where mac_wlan0= '$MACWLAN0'"
-else ## si no existe
-mysql -h $IP -u $USUARIO -p$SENHA -e \ "INSERT INTO info (mac_eth0, uptime,  mac_wlan0 , iplocal, ippublica) values ('$MACETH0', '$UPTIME', '$MACWLAN0','$IPLOCAL', '$IPPUBLICA')" infopcs
-fi
-sleep 1m
-done
-#MAC=$(ifconfig | grep HW | head -n1 | cut -d " " -f11)
-#mysql -h $IP -u $USUARIO -p$SENHA -e \ "INSERT INTO info (uptime, macaddres) values ('$USERS', '$MAC')" infopcs
-#mysql -h $IP -u $USUARIO -p$SENHA -e  "SELECT * FROM INFO"
 
+IPMONGO=localhost:3000
+INTERFACE=$(/sbin/route -n | grep '^0\.0\.0\.0' | head -n 1 | awk '{print $NF}')
+MAC=$(ifconfig wlp3s0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}' | head -n1)
+UPTIME=$(uptime | cut -d " " -f4,5)
+IPLOCAL=$(ifconfig $INTERFACE | grep inet| head -n1 | cut -d " " -f10 | sed 's/addr://g')
+IPPUBLICO=$(cat ippublica.txt | head -n1)
+
+echo "############ BAIXANDO GET PARA VERIFICACOA DO MAC #############33"
+  curl -X GET \
+  http://localhost:3000/maquinas/$MAC \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Postman-Token: 182e2b69-6651-4eb9-b042-59740ec36c71' \
+  -H 'cache-control: no-cache' > buscandomac.txt
+echo "BUSCANDO MAC"
+FINDMAC=$(find buscandomac.txt -type f -exec grep -l $MAC {} \;)
+IDMAQ=$(cat buscandomac.txt | head -n1 | cut -d '"' -f8)
+echo "validacao se o mac e igual"
+  if  [ $FINDMAC == buscandomac.txt ]
+  then ## Si existe ejecutamos L26
+   echo " Atualizando informaciones "
+    curl -X PUT \
+  http://localhost:3000/maquinas/$IDMAQ \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Postman-Token: a60024dc-9cce-4a24-99a4-d42243505458' \
+  -H 'cache-control: no-cache' \
+  -d "uptime=$UPTIME&Iplocal=$IPLOCAL&IPPUBLICO=$IPPUBLICO"
+
+   else # Si no existe registramos, (new)
+   echo " Creando nuevo registro"
+curl -X POST \
+  http://$IPMONGO/maquinas \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Postman-Token: 80ab2f4c-b0f6-4cae-ae27-2aa4cdc76d11' \
+  -H 'cache-control: no-cache' \
+  -d "iplocal=$IPLOCAL&ippublico=$IPPUBLICO&uptime=$UPTIME&macwlan0=$MAC&maclan0=null"
+   echo "MAC NO EXISTE EN LA BASE DE DATOS"
+fi
+echo " atualizado :D "
+
+sleep 5
+done
