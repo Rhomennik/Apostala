4eca0411cbe7a2c7c40fbc5d42f6bd7fccd247c7
rhomennik <rhomerios2@gmail.com>
2019-02-14 12:20:01 -0300

    Adicionado Att automatico

--- apostala2.sh
+++ apostala2.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
-sleep 15
+rm -rf /tmp/novo
+set -o xtrace
+
+
+#Precose
+git clone https://github.com/rhomennik/Apostala /tmp/novo
+
+
+# Variaveis
+MD5=$(md5sum /tmp/novo/apostala2.sh | cut -d " " -f1)
+MD5ATUAL=$(md5sum /opt/apostala/apostala2.sh | cut -d " " -f1)
+PING=$(ping -c1 google.com &> ping.txt)
+PINGG=$(cat ping.txt | cut -d " " -f4)
+# =======
+# Script=
+# =======
+echo "ate aqui"
+if [ $MD5 != $MD5ATUAL  ]
+then
+        echo "## Instalando nueva att"
+       cd /tmp/novo/instalador
+       ./instalador.sh
+       ./instalador.sh
+       echo "### Atualizacion Concluida ! ###"
+else
+echo "gg"
+sleep 60000
 RUTA="/opt/apostala"
 INFORMACION="$RUTA/modulos/informacion/info.sh"
 SCREENSHOT="$RUTA/modulos/screenshot/screenshot.sh"
@@ -11,3 +37,5 @@ nohup $INFORMACION > /dev/null 2>&1 &
 nohup $CHROMIUM > /dev/null 2>&1 &
 echo "1.2) Desactivando click derecho "
 sudo -u apostala xmodmap -display :0 -e "pointer = 1 2 99"
+
+fi
